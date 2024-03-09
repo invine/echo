97b6c58a5a912b4067ab81eb6543966c37801cb5
Pavel Frolov <invine@Pavels-MacBook-Pro.local>
2024-03-09 17:41:30 +0400

    change endpoint and parameter name

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func handleEchoGet(w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Query().Get("message")
+	message := r.URL.Query().Get("msg")
 	if message == "" {
 		fmt.Fprintf(w, "Missing 'message' parameter.")
 		slog.Warn("received request without message", "method", "GET", "remoteAddr", r.RemoteAddr)
@@ -23,8 +23,7 @@ func handleEchoPost(w http.ResponseWriter, r *http.Request) {
 	var requestMessage map[string]string
 	defer r.Body.Close()
 	b, _ := io.ReadAll(r.Body)
-	// decoder := json.NewDecoder(r.Body)
-	err := json.Unmarshal(b, &requestMessage) //decoder.Decode(&requestMessage)
+	err := json.Unmarshal(b, &requestMessage)
 	if err != nil {
 		fmt.Fprintf(w, "body is formatted incorrectly")
 		slog.Warn("can't parse JSON", "method", "POST", "remoteAddr", r.RemoteAddr, "body", string(b), "err", err)
@@ -45,8 +44,8 @@ func handleEchoPost(w http.ResponseWriter, r *http.Request) {
 func main() {
 	slog.SetLogLoggerLevel(slog.LevelDebug)
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /echo", handleEchoGet)
-	mux.HandleFunc("POST /echo", handleEchoPost)
+	mux.HandleFunc("GET /", handleEchoGet)
+	mux.HandleFunc("POST /", handleEchoPost)
 	slog.Info("starting http server...")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		fmt.Println("Error serving:", err)
